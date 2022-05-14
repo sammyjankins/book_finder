4c89ed7bbc01190434f1f3ce6ff0e3bddd8842f9
SammyJankins <neroragno@mail.ru>
2022-05-15 02:21:55 +0700

    second

--- book_finder/settings.py
+++ book_finder/settings.py
@@ -127,6 +127,9 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
