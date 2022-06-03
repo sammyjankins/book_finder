f65ae8edd3e5598033a411e3e4cd087601ec4843
SammyJankins <neroragno@mail.ru>
2022-06-04 00:48:37 +0700

    global vars port

--- parse_server.py
+++ parse_server.py
@@ -11,7 +11,7 @@ class SQManager(BaseManager):
 def handle():
     SQManager.register('get_request_collector', callable=lambda: request_collector)
     SQManager.register('get_output_collector', callable=lambda: output_collector)
-    server_manager = SQManager(address=('', 50000), authkey=b'qwerasdf')
+    server_manager = SQManager(address=('', 68), authkey=b'qwerasdf')
     server = server_manager.get_server()
     print(server.address)
     server.serve_forever()
