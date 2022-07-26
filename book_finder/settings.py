19084659277a35f76fcc70e46452eb313353ba3d
SammyJankins <neroragno@mail.ru>
2022-07-27 00:57:42 +0700

    changed some settings

--- book_finder/settings.py
+++ book_finder/settings.py
@@ -19,10 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v(3l=kz2pxd^@=*&u(kg$yp%2qd8s80c527m(qa%r+u7(nu%&='
+SECRET_KEY = os.environ.get("SECRET_KEY")
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
